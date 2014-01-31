1c2db8906e728a13539178bf30c28717c4b14619
senthil8kmiles <senthilkumar@8kmiles.com>
2014-01-31 12:50:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // Show all information, defaults to INFO_ALL
 1phpinfo();
-dddddddd
+ddddddddjjjjjjjjjjjjjjjjjj
 // Show just the module information.
 // phpinfo(8) yields identical results.
 phpinfo(INFO_MODULES);
